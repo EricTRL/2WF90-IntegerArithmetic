e6b395e67c91ae7b7d3c58f9c6404f7e309002da
Jonker <r.m.jonker@student.tue.nl>
2018-09-06 11:50:19 +0200

    Base files for testing added
    All still empty

---
+++ tests/io/InputReaderTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io;
+
+/**
+ * @author E.M.A. Arts (1004076)
+ * @author K. Degeling (1018025)
+ * @author R.M. Jonker (1011291)
+ * @author S. Jacobs (1005276)
+ * @author M. Schotsman (0995661)
+ * 
+ * @since 6 SEPTEMBER 2018
+ */
+public class InputReaderTest {
+    
+}
