ff4f8fe6c68e33fc97ced28165a5ab90c6468a75
Dodecahedra <k.degeling@student.tue.nl>
2018-09-06 12:27:07 +0200

    Implemented addition with radix b.

--- src/arithmetic/Addition.java
+++ src/arithmetic/Addition.java
@@ -21,5 +21,39 @@ package arithmetic;
  * @since 6 SEPTEMBER 2018
  */
 public class Addition {
+    public static void main(String[] args) {
+        int[] a = {3};
+        int[] b = {8};
+        for (int x : addition(a, b, 10)) {
+            System.out.print(x);
+        }
+    }
+    /**
+     * Adds to large numbers in radix b
+     *
+     * @param x array with first number
+     * @param y array with second number
+     * @param b radix to be used
+     * @pre x.length==y.length
+     * @return x + y in radix b
+     */
+    public static int[] addition (int[] x, int[] y, int b) {
+        int carry = 0; //optional carry we need to add.
+        int[] answer= new int[x.length+1];
+        for (int i = answer.length-1; i > 0; i--) {
+            answer[i] = (x[i-1] + y[i-1] + carry)%b; //compute new value
+
+            //compute carry
+            if (x[i-1]+y[i-1]+carry>=b) {
+                carry = (x[i-1]+y[i-1]+carry)/b;
+            } else {
+                carry = 0;
+            }
+        }
+        if (carry>0) { //Deal with potential 'overflow'
+            answer[0] = carry;
+        }
+        return answer;
+    }
     
 }
