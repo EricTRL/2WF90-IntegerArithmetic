3edee54566af4d162732e8acd184eb9094b390f8
Dodecahedra <k.degeling@student.tue.nl>
2018-09-20 10:40:12 +0200

    ...

--- src/arithmetic/Modulo.java
+++ src/arithmetic/Modulo.java
@@ -104,7 +104,7 @@ public class Modulo {
         LinkedList<Integer> x_prime = x; LinkedList<Integer> m_prime = m;
         int x_1 = 1; int x_2 = 0;
         while (Arithmetic.isPositive(m_prime)) {
-            LinkedList<Integer> q = Division.divide(x_prime, m_prime, b, null).q;
+            LinkedList<Integer> q = Division.dumbDivide(x_prime, m_prime, b, null).q;
             LinkedList<Integer> r = Subtraction.subtract(x_prime, Karatsuba.karatsuba(q, m_prime, b, null, 1), b, null);
             x_prime = m_prime; m_prime = r;
 
